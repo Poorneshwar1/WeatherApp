c087d849ee60c189097bf110b6952312889b524c
Poorneshwar1 <poorneshwarreddyba@gmail.com>
2023-07-19 14:45:42 +0530

    ok

---
+++ src/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+<>
+
+<section className='flex w-full h-screen justify-center items-center bg-gray-100 '>
+
+
+
+</section>
+
+</>
+
+  )
+}
+
+export default Loading
